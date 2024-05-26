ea935d1b6f6bcbd22a50e61f41f43b5635bc4d49
snowin <81621539+snowin-jj@users.noreply.github.com>
2024-05-26 14:47:41 +0530

    Delete dist directory

--- dist/postcss.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    plugins: {
-        tailwindcss: {},
-        autoprefixer: {},
-    },
-};
-//# sourceMappingURL=postcss.config.js.map
--- dist/src/renderer/pages/admin/AdminDashboard.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const jsx_runtime_1 = require("react/jsx-runtime");
-function AdminDashboard() {
-    return ((0, jsx_runtime_1.jsx)("main", { children: (0, jsx_runtime_1.jsx)("h1", { children: "Admin Dashboard" }, void 0) }, void 0));
-}
-exports.default = AdminDashboard;
-//# sourceMappingURL=AdminDashboard.js.map
