ae0fef0495ac485a7e1398b70bf741957817cb7c
snowin-jj <snowin.developer@gmail.com>
2024-06-01 22:13:51 +0530

    add react printer

--- src/renderer/components/bill.tsx
+++ src/renderer/components/bill.tsx
@@ -1,6 +1,6 @@
 import { OrderWithDetails } from '@/types/order';
 import { forwardRef, useRef } from 'react';
-import { useReactToPrint } from 'react-to-print';
+import ReactToPrint, { useReactToPrint } from 'react-to-print';
 import { Button } from './ui/button';
 import {
     Dialog,
@@ -12,6 +12,24 @@ import {
 interface BillProps {
     order: OrderWithDetails;
 }
+
+const pageStyle = `
+@page {
+    size: 80mm 20mm;
+};
+
+@media all {
+    .pageBreak {
+        display: none;
+    }
+};
+
+@media print {
+    .pageBreak {
+        page-break-before: always;
+    }
+}
+`;
 
 export default function Bill({ order }: BillProps) {
     const componentRef = useRef(null);
@@ -26,16 +44,17 @@ export default function Bill({ order }: BillProps) {
                 <Button className="w-full">Bill</Button>
             </DialogTrigger>
             <DialogContent>
-                <div
-                    ref={componentRef}
-                    className={`print-area h-[500px] w-[400px] bg-slate-500`}
-                >
-                    <h2>Printable</h2>
-                </div>
+                <PrintablePart ref={componentRef} />
                 <DialogFooter>
-                    <Button className="w-full" onClick={handlePrint}>
-                        Print
-                    </Button>
+                    <ReactToPrint
+                        trigger={() => (
+                            <Button className="w-full" onClick={handlePrint}>
+                                Print
+                            </Button>
+                        )}
+                        content={() => componentRef.current}
+                        pageStyle={pageStyle}
+                    />
                 </DialogFooter>
             </DialogContent>
         </Dialog>
@@ -49,10 +68,30 @@ const PrintablePart = forwardRef<HTMLDivElement, PrintablePartProps>(
         return (
             <div
                 ref={ref}
-                className={`h-[500px] w-[400px] bg-slate-500 ${className}`}
+                className={`h-[500px] w-[400px] bg-zinc-900 ${className}`}
                 {...props}
             >
                 <h2>Printable</h2>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Voluptates quidem, animi modi vitae, corporis est ullam
+                    ipsam rerum mollitia, nostrum ratione facilis perferendis
+                    amet dolor officia accusamus obcaecati ut repellat in nemo
+                    laudantium ex itaque unde illo. Quaerat praesentium nisi
+                    harum ab molestias reprehenderit omnis doloremque nihil, sed
+                    consequatur illum eius error dolor necessitatibus non
+                    excepturi, voluptatibus atque. Sed ullam ipsam ipsa aliquid
+                    illo mollitia autem at consequatur laudantium, id similique
+                    esse aliquam alias placeat. Assumenda soluta ad maxime non
+                    laudantium, minus asperiores eligendi mollitia in voluptatem
+                    vitae expedita nihil quae, sapiente quis obcaecati. Sit
+                    molestiae ipsa modi numquam facilis. esse aliquam alias
+                    placeat. Assumenda soluta ad maxime non laudantium, minus
+                    asperiores eligendi mollitia in voluptatem vitae expedita
+                    nihil quae, sapiente quis obcaecati. Sit molestiae ipsa modi
+                    numquam facilis. placeat. Assumenda soluta ad maxime non
+                    laudantium.
+                </p>
             </div>
         );
     },
