7b485a894f77c9ae9dd55a387a2f7c15c4f1dca6
snowin-jj <snowin.developer@gmail.com>
2024-08-01 20:03:10 +0530

    add: platform support

--- forge.config.ts
+++ forge.config.ts
@@ -56,6 +56,7 @@ const config: ForgeConfig = {
     publishers: [
         {
             name: '@electron-forge/publisher-github',
+            platforms: ['darwin', 'linux', 'win32'],
             config: {
                 repository: {
                     owner: 'snowin-jj',
