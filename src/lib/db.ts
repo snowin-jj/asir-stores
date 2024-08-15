297bde4074d9c031863070b01dcc993ef7e243ec
snowin-jj <snowin.developer@gmail.com>
2024-08-16 03:33:46 +0530

    include resources only in production

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -5,15 +5,13 @@ import { ENV } from '../utils/constants';
 
 const knex = Knex(config[ENV]);
 
-knex.migrate
-    .latest()
-    .then(() => {
-        console.log('Migration completed!');
-        // knex.seed
-        //     .run()
-        //     .then(() => console.log(`Seeded successfully`))
-        //     .catch((err) => console.log('Error in seeding: ', err));
-    })
-    .catch((err) => console.error('Error in migration: ', err));
+if (ENV === 'production') {
+    knex.migrate
+        .latest()
+        .then(() => {
+            console.log('Migration completed!');
+        })
+        .catch((err) => console.error('Error in migration: ', err));
+}
 
 export default knex;
